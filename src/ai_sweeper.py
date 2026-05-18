d8b7317183503341fe53b710074383f4f7a0ebfc
wesley <weslleycontas09@gmail.com>
2026-05-17 23:12:09 -0300

    Entrega final do AI Sweeper

--- src/ai_sweeper.py
+++ src/ai_sweeper.py
@@ -1,6 +1,79 @@
-# solução final 
+import numpy as np
+import pandas as pd
+
+def extrair_features(tabuleiro, x, y): #Transformar uma célula do jogo em uma linha igual ao dataset. tambem recebe tabuleiro atual e coordenadas da célula.
+    features = [] # aqui vai armazenar os valores das células vizinhas
+    # percorre a janela 5x5 ao redor da célula
+    for dx in range(-2, 3):
+        for dy in range(-2, 3):
+
+            # ignora a célula central
+            if dx == 0 and dy == 0:
+                continue
+
+            nx = x + dx # Calculo da celula vizinha 
+            ny = y + dy
+
+            # se sair do tabuleiro → usa -2
+            if (
+                nx < 0
+                or ny < 0
+                or nx >= tabuleiro.shape[0]
+                or ny >= tabuleiro.shape[1]
+            ):
+                features.append(-2)
+            else:   
+                features.append(tabuleiro[nx][ny])
+
+    
+
+
+    total_celulas = tabuleiro.size
+
+    minas_suspeitas = np.sum(tabuleiro == -1)
+
+    global_density = minas_suspeitas / total_celulas
+
+    features.append(global_density)
+
+    colunas = [
+    "cell_-2_-2", "cell_-2_-1", "cell_-2_0", "cell_-2_1", "cell_-2_2",
+    "cell_-1_-2", "cell_-1_-1", "cell_-1_0", "cell_-1_1", "cell_-1_2",
+    "cell_0_-2", "cell_0_-1", "cell_0_1", "cell_0_2",
+    "cell_1_-2", "cell_1_-1", "cell_1_0", "cell_1_1", "cell_1_2",
+    "cell_2_-2", "cell_2_-1", "cell_2_0", "cell_2_1", "cell_2_2",
+    "global_density"
+]
+
+    return pd.DataFrame([features], columns=colunas)
+
+
 def prever_jogada_segura(tabuleiro_atual, modelo):
-    # 1. Faça o pré-processamento do tabuleiro_atual (ETL)
-    # 2. Peça ao 'modelo' para fazer a predição (.predict)
-    # 3. Retorne a coordenada X e Y mais segura
-    return (x, y)
+
+
+    tabuleiro = np.array(tabuleiro_atual)
+
+    melhor_probabilidade = -1
+
+    melhor_jogada = None
+
+    for x in range(tabuleiro.shape[0]):
+        for y in range(tabuleiro.shape[1]):
+
+            if tabuleiro[x][y] == -1:
+
+                features = extrair_features(tabuleiro, x, y)
+
+                probabilidade_segura = modelo.predict_proba(features)[0][1]
+
+                if probabilidade_segura > melhor_probabilidade:
+
+                    melhor_probabilidade = probabilidade_segura
+
+                    melhor_jogada = (x, y)
+
+    if melhor_jogada is None:
+        fechadas = list(zip(*np.where(tabuleiro == -1)))
+        return fechadas[0] if fechadas else (0, 0)
+    
+    return melhor_jogada
--- starter_kits/02_nivel_scikit_learn.py
+++ starter_kits/02_nivel_scikit_learn.py
@@ -1,38 +1,79 @@
 # ==============================================================================
 # 🚀 STARTER KIT: SCIKIT-LEARN (Machine Learning Clássico)
-# Missão: Utilizar modelos tradicionais (Árvores, Regressão, Random Forest)
 # ==============================================================================
 import pandas as pd
+import joblib
+
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
-# TODO: Importe o seu modelo (ex: from sklearn.ensemble import RandomForestClassifier)
+from sklearn.ensemble import RandomForestClassifier
+
 
 def pipeline_scikit_learn(caminho_dados):
-    # 1. ETL (Extração e Limpeza)
+
+    # 1. ETL (Extração e Transformação)
     print("1. Carregando dados...")
-    # df = pd.read_csv(caminho_dados)
-    
-    # TODO: Separe as Features (X) do Target (y)
-    X = [] 
-    y = []
-    
-    # 2. Divisão de Treino e Teste (Obrigatório em ML)
-    # X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
+
+    df = pd.read_csv(caminho_dados)
+
+    # amostragem aleatória para evitar viés do dataset
+    df = df.sample(n=1300000, random_state=42)
+
+    # separação entre features e target
+    X = df.drop("safe", axis=1)
+    y = df["safe"]
+
+
+    # 2. Divisão de Treino e Teste
+    X_train, X_test, y_train, y_test = train_test_split(
+        X,
+        y,
+        test_size=0.2,
+        random_state=42
+    )
+
+
     # 3. Instanciação e Treinamento
     print("2. Treinando o modelo...")
-    # modelo = RandomForestClassifier()
-    # modelo.fit(X_train, y_train)
-    
-    # 4. Avaliação (O Teste Cego)
+
+    modelo = RandomForestClassifier(
+        n_estimators=150,
+        max_depth=None,
+        min_samples_split=15,
+        min_samples_leaf=8,
+        random_state=42,
+        n_jobs=-1
+        )
+
+    modelo.fit(X_train, y_train)
+
+
+    # análise de generalização
+    train_acc = modelo.score(X_train, y_train)
+    test_acc = modelo.score(X_test, y_test)
+
+    print(f"Acurácia treino: {train_acc * 100:.2f}%")
+    print(f"Acurácia teste: {test_acc * 100:.2f}%")
+
+
+    # 4. Avaliação
     print("3. Avaliando o modelo...")
-    # previsoes = modelo.predict(X_test)
-    # acuracia = accuracy_score(y_test, previsoes)
-    # print(f"Acurácia no Teste: {acuracia * 100:.2f}%")
-    
-    # return modelo
+
+    previsoes = modelo.predict(X_test)
+
+    acuracia = accuracy_score(y_test, previsoes)
+
+    print(f"Acurácia final: {acuracia * 100:.2f}%")
+
+    joblib.dump(modelo, "modelo_minesweeper.pkl")
+    print("Modelo salvo com sucesso!! como 'modelo_minesweeper.pkl'")
+
+    return modelo
+
 
 # --- ÁREA DE TESTE ---
 if __name__ == "__main__":
-    # modelo_pronto = pipeline_scikit_learn("dados.csv")
-    print("Boilerplate Scikit-Learn pronto!")
+
+    modelo_pronto = pipeline_scikit_learn(
+        "dataset/minesweeper_dataset/minesweeper_dataset.csv"
+    )
